77a39dfaac390a6312c564da797e7b5284617bd6
Matthias Meckel <MEMeckel@UCLAN.AC.UK>
2018-02-20 08:27:15 +0000

    Initial commit

---
+++ visualisation.R
@@ -0,0 +1,25 @@
+# First, set the correct Working Directory. (write your code in the line below)
+
+setwd()
+
+# Next, we are creating a new vector
+data <- c("Below Average", "Average", "Above Average", "Excellent", "Above Average", "Above Average", "Above Average", "Below Average", "Average", "Poor", "Below Average","Average","Poor", "Above Average", "Average","Above Average","Average", "Below Average","Above Average", "Above Average")
+
+# For the following tasks please always write the code below the comment with the instructions, example:
+#
+# Print out the data vector
+data
+
+# Now it's your turn:
+#
+# Produce a frequency distribution table for the data vector and print out this table
+
+
+# Plot a bar chart. Make it more presentable by labelling it properly. The bars should be ordered from Poor to Excellent.
+
+
+# Plot a 3D pie chart. Make it more presentable by labelling it properly. The slices should be ordered from Poor to Excellent.
+
+
+# That's all. Don't forget to push your commit.
+# Just a reminder that you have to work on this task by yourself. If students submit a similar solution it will be easy to find out in GitHub which student pushed their commit first.
